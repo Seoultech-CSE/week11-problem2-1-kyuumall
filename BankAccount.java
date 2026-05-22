7fe62a83cde7046248c4af2add4c4a4755262ff8
kyuumall <iqmalhazeeq321@gmail.com>
2026-05-22 06:01:26 +0000

    Modify the output based on the Case 1: Normal Flow expected outcome; Remove the opening and closing statement alongside with the inital balance at first; Remove the prompts of asking amount of deposit and withdraw; change the line of Current Balance: $ to [Current Balance Status] $

--- BankAccount.java
+++ BankAccount.java
@@ -40,7 +40,7 @@ public class BankAccount {
     public void deposit(double amount) throws IllegalArgumentException {
         if (amount > 0){     
             balance += amount;
-            System.out.println("$" + amount + " was successfully deposited.");
+            System.out.println("$" + amount + " successfully deposited.");
 
         } else {
             throw new IllegalArgumentException("$" + amount + " was unsucessfully deposited.");
@@ -53,7 +53,7 @@ public class BankAccount {
     public void withdraw(double amount) throws InsufficientBalanceException {
         if (amount > 0 && amount <= balance){
             balance -= amount;
-            System.out.println("$" + amount + " was successfully withdrawn.");
+            System.out.println("$" + amount + " successfully withdrawn.");
 
         } else {
             throw new InsufficientBalanceException(balance, amount);
@@ -64,15 +64,11 @@ public class BankAccount {
         Scanner input = new Scanner(System.in);
         BankAccount account = new BankAccount(500.0); 
 
-        System.out.println("=== Welcome to the Interactive Banking System ===");
-        System.out.println("Initial Balance: $500.0");
-
         // --- DEPOSIT PROCESS ---
         // TODO: Wrap the deposit process in a try-catch-finally layout[cite: 34].
         // Catch InputMismatchException and IllegalArgumentException, and always display the balance[cite: 44, 151].
 
         try{
-            System.out.print("\nEnter the amount to DEPOSIT: ");
             double depositAmount = input.nextDouble();
             account.deposit(depositAmount);
 
@@ -84,7 +80,7 @@ public class BankAccount {
             System.out.println("Error: " + error.getMessage());
 
         } finally {
-            System.out.println("Current Balance: $" + account.getBalance());
+            System.out.println("[Current Balance Status] $" + account.getBalance());
         }
 
 
@@ -93,7 +89,6 @@ public class BankAccount {
         // Catch InputMismatchException and InsufficientBalanceException, and always display the balance[cite: 44, 151].
 
         try{
-            System.out.print("\nEnter the amount to WITHDRAW: ");
             double withdrawAmount = input.nextDouble();
             account.withdraw(withdrawAmount);
 
@@ -105,11 +100,9 @@ public class BankAccount {
             System.out.println("Error: " + error.getMessage());
 
         } finally {
-            System.out.println("Current Balance: $" + account.getBalance());
+            System.out.println("[Current Balance Status] $" + account.getBalance());
         }
         
-
-        System.out.println("\n=== Thank you for using our service ===");
         input.close();
     }
 }
